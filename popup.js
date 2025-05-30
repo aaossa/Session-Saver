48d30ecf72ececb5668cf2403d9652dd6dc3a903
Antonio Ossa Guerra <aaossa@uc.cl>
2025-05-30 12:00:43 -0400

    :sparkles: Add select all checkbox
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- popup.js
+++ popup.js
@@ -20,15 +20,51 @@ async function saveSessions(sessions) {
 function renderTabs(tabs) {
   const container = document.getElementById('tabs');
   container.innerHTML = '';
+
+  // Select/Unselect all checkbox row
+  const selectAllRow = document.createElement('div');
+  selectAllRow.className = 'select-all-row';
+  selectAllRow.innerHTML = `
+    <input type="checkbox" id="selectAllTabs" checked style="margin-right:8px;" />
+    <label for="selectAllTabs" class="select-all-label">Select/Unselect All</label>
+  `;
+  container.appendChild(selectAllRow);
+
+  // Individual tab checkboxes
   tabs.forEach(tab => {
     const div = document.createElement('div');
+    div.className = 'tab-row';
     div.innerHTML = `
-      <input type="checkbox" id="tab-${tab.id}" checked />
+      <input type="checkbox" class="tab-checkbox" id="tab-${tab.id}" checked />
       <label for="tab-${tab.id}" title="${tab.url}">
         ${tab.title}
       </label>
     `;
     container.appendChild(div);
+  });
+
+  // Handler for "Select/Unselect All"
+  const selectAllBox = document.getElementById('selectAllTabs');
+  selectAllBox.addEventListener('change', () => {
+    const allChecked = selectAllBox.checked;
+    tabs.forEach(tab => {
+      document.getElementById(`tab-${tab.id}`).checked = allChecked;
+    });
+  });
+
+  // Handler for individual checkboxes to update "Select/Unselect All"
+  tabs.forEach(tab => {
+    const checkbox = document.getElementById(`tab-${tab.id}`);
+    checkbox.addEventListener('change', () => {
+      const allChecked = tabs.every(tab2 => document.getElementById(`tab-${tab2.id}`).checked);
+      selectAllBox.checked = allChecked;
+      // If at least one is unchecked, but not all, make selectAllBox indeterminate
+      if (!allChecked && tabs.some(tab2 => document.getElementById(`tab-${tab2.id}`).checked)) {
+        selectAllBox.indeterminate = true;
+      } else {
+        selectAllBox.indeterminate = false;
+      }
+    });
   });
 }
 
